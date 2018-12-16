07df48ad0c90d28ad2768867335f0f380d47c0cb
Chase Southard <chase.southard@gmail.com>
2018-12-15 19:33:17 -0500

    adds a first pass at refactoring the handler
    not in its final form, but the basic idea is there.
    get a list of teams; get the calendar data for each team; send to the
    confirmed recipients on each team

--- app.rb
+++ app.rb
@@ -1,6 +1,21 @@
 require 'json'
 require_relative 'messenger'
+require_relative 'person'
 
 def lambda_handler(event:, context:)
-  Messenger.new.outgoing
+
+  [
+    { calendar_id: 'fayette.kyschools.us_jtcs0efrcacc9c8pk9e303278c%40group.calendar.google.com', team: 'voyagers_6' }
+  ].each do |entry|
+    response = Calendar.get(entry[:calendar_id])
+    if response.code != 200
+      raise "Failed to get a successful response from Google Calendar. #{response.inspect}"
+    end
+
+    recipients = Person.find_confirmed_by_team(entry[:team]).entries.map(&:email_address)
+    puts recipients.inspect
+
+    Messenger.new.outgoing(calendar_data: JSON.parse(response), recipient_list: recipients)
+  end
+
 end
