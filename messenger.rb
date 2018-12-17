d27a57a20e815c07ec763f42c7fafc40d9625bed
Chase Southard <chase.southard@gmail.com>
2018-12-16 21:17:05 -0500

    refactor the messenger to use the batch mailer

--- messenger.rb
+++ messenger.rb
@@ -3,22 +3,26 @@ require 'json'
 require 'active_support/all'
 require_relative 'calendar'
 require_relative 'template'
-require_relative 'mailer'
+require_relative 'batch_mailer'
 
 class Messenger
   def outgoing(calendar_data:, recipient_list:)
     message = studies_message(calendar_data)
-    mailer = Mailer.new(to: "chase.southard@gmail.com",
+    mailer = BatchMailer.new(recipient_list: recipient_list,
                         from: "no-reply@assignmentsbot.com",
-                        subject: "Assignments for #{Date.today.to_s}",
+                        subject: "#{calendar_summary(calendar_data)} Assignments for #{Date.today.to_s}",
                         text: message)
-    mailer.deliver
+    mailer.deliver_batch
   end
 
   private
 
+  def calendar_summary(calendar_data)
+    calendar_data.dig('summary')
+  end
+
   def studies_message(calendar_data)
-    Template.new(summary: calendar_data.dig('summary'), assignments: assignments(calendar_data)).output
+    Template.new(summary: calendar_summary(calendar_data), assignments: assignments(calendar_data)).output
   end
 
   def assignments(hsh)
