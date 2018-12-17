86b56a9805d340cb6b073aaec7747134b790ad08
Chase Southard <chase.southard@gmail.com>
2018-12-16 21:20:24 -0500

    add refactoring to the lambda handler
    in an effor to be more dynamic

--- app.rb
+++ app.rb
@@ -1,21 +1,20 @@
 require 'json'
 require_relative 'messenger'
 require_relative 'person'
+require_relative 'team'
 
 def lambda_handler(event:, context:)
-
-  [
-    { calendar_id: 'fayette.kyschools.us_jtcs0efrcacc9c8pk9e303278c%40group.calendar.google.com', team: 'voyagers_6' }
-  ].each do |entry|
-    response = Calendar.get(entry[:calendar_id])
+  Team.all.each do |team|
+    puts "Getting data for #{team.name} at #{team.calendar_id}"
+    response = Calendar.get(team.calendar_id)
     if response.code != 200
       raise "Failed to get a successful response from Google Calendar. #{response.inspect}"
     end
 
-    recipients = Person.find_confirmed_by_team(entry[:team]).entries.map(&:email_address)
+    recipients = Person.find_confirmed_by_team(team.name).entries.map(&:email_address)
     puts recipients.inspect
-
-    Messenger.new.outgoing(calendar_data: JSON.parse(response), recipient_list: recipients)
+    if recipients.any?
+      Messenger.new.outgoing(calendar_data: JSON.parse(response.body), recipient_list: recipients)
+    end
   end
-
 end
